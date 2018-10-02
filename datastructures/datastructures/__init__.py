09ce2164b437ffe85eeedbb269c08198166e5663
Xavier Martinet <xavier.martinet@gmail.com>
2018-10-02 16:18:19 +0200

    starting sorting algorithms

--- datastructures/datastructures/__init__.py
+++
@@ -1,7 +0,0 @@
-from .linked_list import LinkedList
-from .doubly_linked_list import DoublyLinkedList
-from .stack import Stack, StackEmptyError
-from .queue import Queue, QueueEmptyError
-from .binary_search_tree import BinarySearchTree
-from .dictionnary import Dictionnary
-from .priority_queue import PriorityQueue
