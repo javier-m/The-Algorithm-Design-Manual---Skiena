726c0fa5140700308d72489dd60f98fc29c3e376
Xavier Martinet <xavier.martinet@gmail.com>
2018-10-16 17:25:46 +0200

    implement permutations

--- skiena/backtracking/applications.py
+++ skiena/backtracking/applications.py
@@ -3,7 +3,7 @@ from typing import Sequence, List, Any, Iterator
 from .backtracking import backtrack
 
 
-def construct_all_subsets(myset: Sequence[Any]) -> Sequence[List[Any]]:
+def construct_all_subsets(myset: Sequence[Any]) -> Iterator[List[Any]]:
     mylist = list(myset)
     nb_items = len(mylist)
 
@@ -19,3 +19,25 @@ def construct_all_subsets(myset: Sequence[Any]) -> Sequence[List[Any]]:
                                        is_a_solution=is_a_solution,
                                        construct_candidates=construct_candidates):
         yield [x for i, x in enumerate(mylist) if belongs_to_subset[i]]
+
+
+def construct_all_permutations(myset: Sequence[Any]) -> Iterator[List[Any]]:
+    mylist = list(myset)
+    nb_items = len(mylist)
+
+    def is_a_solution(a: list, k: int, inputs: None):
+        nonlocal nb_items
+        return nb_items == k + 1
+
+    def construct_candidates(a: list, k: int, inputs: None) -> Iterator:
+        in_permutation = [False] * nb_items
+        for i in range(k):
+            in_permutation[a[i]] = True
+        for i in range(nb_items):
+            if not in_permutation[i]:
+                yield i
+
+    for permutation_order in backtrack(a=[None] * nb_items,
+                                       is_a_solution=is_a_solution,
+                                       construct_candidates=construct_candidates):
+        yield[mylist[i] for i in permutation_order]
--- skiena/tests/backtracking/test_applications.py
+++ skiena/tests/backtracking/test_applications.py
@@ -3,9 +3,10 @@ from backtracking.applications import *
 
 def test_subset_construction():
     nb_items = 3
+    nb_subsets = 2**3
     a = range(nb_items)
     subsets = list(construct_all_subsets(a))
-    assert len(subsets) == 2**nb_items
+    assert len(subsets) == nb_subsets
     actual_subsets = [
         [],
         [0], [1], [2],
@@ -14,3 +15,23 @@ def test_subset_construction():
     ]
     for subset in subsets:
         assert subset in actual_subsets
+
+
+def test_permutation_construction():
+    nb_items = 3
+    nb_permutations = 1
+    for i in range(nb_items):
+        nb_permutations *= (i+1)
+    a = range(nb_items)
+    permutations = list(construct_all_permutations(a))
+    assert len(permutations) == nb_permutations
+    actual_permutations = [
+        [0, 1, 2],
+        [0, 2, 1],
+        [1, 0, 2],
+        [1, 2, 0],
+        [2, 0, 1],
+        [2, 1, 0],
+    ]
+    for permutation in permutations:
+        assert permutation in actual_permutations
